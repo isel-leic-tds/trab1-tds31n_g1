0b574285384118aae86ed1cb9246344b650628f4
Miguel Rocha <migasrocha1@hotmail.com>
2022-01-04 15:55:45 +0000

    Check message fixed!

--- Chess4DesktopConsole/src/main/kotlin/Commands/Commands.kt
+++ Chess4DesktopConsole/src/main/kotlin/Commands/Commands.kt
@@ -42,8 +42,8 @@ fun restoreGame(chessDb: ChessDb, gameId: String?): CommandResult {
     val list = moves.content.trim().split(" ").toList()
     var statusGame = StatusGame(newBoard,list,Player.WHITE, null)
     list.forEach{ move: String ->
-        var result = statusGame.board!!.makeMoveWithoutCheck(move)
-        var board = result.board
+        val result = statusGame.board!!.makeMoveWithoutCheck(move)
+        val board = result.board
         val check = result.check
         statusGame = statusGame.copy(board = board, currentPlayer = statusGame.currentPlayer!!.other(), lastMove = move,ckeck = check )
     }
