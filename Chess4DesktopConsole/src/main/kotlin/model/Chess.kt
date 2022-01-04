fba3fe9c23cb804ed8e2ffe27b6ef552fcae6162
47186JoaoSilva <74247183+47186JoaoSilva@users.noreply.github.com>
2022-01-04 16:18:48 +0000

    no changes

--- Chess4DesktopConsole/src/main/kotlin/model/Chess.kt
+++ Chess4DesktopConsole/src/main/kotlin/model/Chess.kt
@@ -2,14 +2,14 @@ package model
 
 import DataBase.ChessDb
 import model.Board.Board
-import model.Board.globalCheck
+
 
 enum class Player {
     WHITE, BLACK;
     fun other() = if (this === WHITE) BLACK else WHITE
 }
 
-data class StatusGame(val board: Board?, val moves: List<String>, val currentPlayer: Player?, val lastMove: String?, val check: Boolean = globalCheck) {
+data class StatusGame(val board: Board?, val moves: List<String>, val currentPlayer: Player?, val lastMove: String?, val check: Boolean = false) {
     override fun toString(): String {
         return board.toString()
     }
